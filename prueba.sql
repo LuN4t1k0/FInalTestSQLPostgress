4147d79fd27cece755e2262cdaaac645399d075d
Cristian Venegas <cristian.venegas@materoceano.com>
2022-11-21 17:11:03 -0300

    task : 3

--- prueba.sql
+++ prueba.sql
@@ -23,8 +23,34 @@ CREATE TABLE pelicula_tags (
 );
 
 --2. Inserta 5 películas y 5 tags, la primera película tiene que tener 3 tags asociados, la segunda película debe tener dos tags asociados. (1 punto)
-Insert into peliculas (nombre, anno) VALUES('Harry Potter : El Pricipe Mestizo', 2009);
-Insert into peliculas (nombre, anno) VALUES('The Martian', 2015);
-Insert into peliculas (nombre, anno) VALUES('Iron Man', 2008);
-Insert into peliculas (nombre, anno) VALUES('Matrix', 1999);
-Insert into peliculas (nombre, anno) VALUES('Star Wars: episodio IV - una nueva esperanza', 1977);
+
+--Peliculas
+INSERT INTO peliculas (nombre, anno) VALUES('Harry Potter : El Pricipe Mestizo', 2009);
+INSERT INTO peliculas (nombre, anno) VALUES('The Martian', 2015);
+INSERT INTO peliculas (nombre, anno) VALUES('Iron Man', 2008);
+INSERT INTO peliculas (nombre, anno) VALUES('Matrix', 1999);
+INSERT INTO peliculas (nombre, anno) VALUES('Star Wars: episodio IV - una nueva esperanza', 1977);
+
+--Tags
+INSERT INTO tags (tag) VALUES ('Aventuras');
+INSERT INTO tags (tag) VALUES ('Ciencia Ficción');
+INSERT INTO tags (tag) VALUES ('Terror');
+INSERT INTO tags (tag) VALUES ('Fantasía');
+INSERT INTO tags (tag) VALUES ('Drama');
+
+--Tabla Intemedia Peliculas_tags
+INSER INTO pelicula_tags (pelicula_id, tag_id) VALUES (1,1);
+INSER INTO pelicula_tags (pelicula_id, tag_id) VALUES (1,2);
+INSER INTO pelicula_tags (pelicula_id, tag_id) VALUES (1,4);
+INSER INTO pelicula_tags (pelicula_id, tag_id) VALUES (2,1);
+INSER INTO pelicula_tags (pelicula_id, tag_id) VALUES (2,2);
+
+--3. Cuenta la cantidad de tags que tiene cada película. Si una película no tiene tags debe
+select
+  peliculas.nombre,
+  count(pelicula_tags.tag_id)
+From
+  peliculas
+  LEFT JOIN pelicula_tags on peliculas.id = pelicula_tags.pelicula_id
+GROUP by
+  peliculas.nombre;
